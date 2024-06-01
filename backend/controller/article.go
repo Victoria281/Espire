fc7088fb0a684f3ab1925e18e176053ae7dca64b
Victoria281 <amandaquek281@gmail.com>
2024-06-01 21:09:50 +0800

    Finished some routes for articles

---
+++ backend/controller/article.go
@@ -0,0 +1,120 @@
+package controller
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/Victoria281/Espire/backend/auth"
+	"github.com/Victoria281/Espire/backend/models"
+	"github.com/Victoria281/Espire/backend/services"
+	"github.com/gofiber/fiber/v2"
+)
+
+type ArticleController struct {
+	Service services.ArticleService
+}
+
+func NewArticleController(service services.ArticleService) *ArticleController {
+	return &ArticleController{Service: service}
+}
+
+func (c *ArticleController) GetArticleByID(ctx *fiber.Ctx) error {
+	fmt.Println("GetArticleByID")
+
+	id := ctx.Params("id")
+	articleID, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid article ID"})
+	}
+
+	article, err := c.Service.FindById(uint(articleID))
+	if err != nil {
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Article not found"})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(article)
+}
+
+func (c *ArticleController) GetArticle(ctx *fiber.Ctx) error {
+	fmt.Println("GetArticle")
+
+	username := auth.ParseUsername(ctx)
+
+	article, err := c.Service.FindByUsername(username)
+	if err != nil {
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Article not found"})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(article)
+}
+
+func (c *ArticleController) GetArticleByUsername(ctx *fiber.Ctx) error {
+	fmt.Println("GetArticleByUsername")
+	username := ctx.Params("username")
+
+	article, err := c.Service.FindByUsername(username)
+	if err != nil {
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Article not found"})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(article)
+}
+
+func (c *ArticleController) GetArticlesByName(ctx *fiber.Ctx) error {
+	fmt.Println("GetArticlesByName")
+
+	name := ctx.Params("name")
+	if name == "" {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Name parameter is missing"})
+	}
+
+	articles, err := c.Service.FindByName(name)
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error fetching articles"})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(articles)
+}
+
+func (c *ArticleController) CreateNewArticle(ctx *fiber.Ctx) error {
+	var article models.Articles
+	username := auth.ParseUsername(ctx)
+	if err := ctx.BodyParser(&article); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
+
+	if err := c.Service.CreateNewArticle(username, article); err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error creating article"})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "Article created successfully"})
+}
+
+func (c *ArticleController) UpdateArticle(ctx *fiber.Ctx) error {
+
+	var article models.Articles
+	if err := ctx.BodyParser(&article); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
+
+	if err := c.Service.UpdateArticle(article); err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error updating article"})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Article updated successfully"})
+}
+
+func (c *ArticleController) DeleteArticle(ctx *fiber.Ctx) error {
+
+	id := ctx.Params("id")
+	articleID, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid article ID"})
+	}
+
+	err = c.Service.DeleteArticle(uint(articleID))
+	if err != nil {
+		return err // Service method errors will be handled by Fiber
+	}
+	return ctx.JSON(fiber.Map{"message": "Article deleted successfully"})
+}
---
+++ backend/controller/articleflashcards.go
@@ -0,0 +1,73 @@
+package controller
+
+import (
+	"strconv"
+
+	"github.com/Victoria281/Espire/backend/models"
+	"github.com/Victoria281/Espire/backend/services"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type ArticleFlashcardController struct {
+	Service services.ArticleFlashcardService
+}
+
+func NewArticleFlashcardController(service services.ArticleFlashcardService) *ArticleFlashcardController {
+	return &ArticleFlashcardController{Service: service}
+}
+
+func (c *ArticleFlashcardController) CreateFlashcard(ctx *fiber.Ctx) error {
+	var createRequest struct {
+		ArticleID string `json:"article_id"`
+		Answer    string `json:"answer"`
+		Question  string `json:"question"`
+		Tries     int    `json:"tries"`
+		Wrong     int    `json:"wrong"`
+	}
+	if err := ctx.BodyParser(&createRequest); err != nil {
+		return err // Fiber will handle parsing errors automatically
+	}
+
+	articleID, err := strconv.ParseUint(createRequest.ArticleID, 10, 64)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid article ID"})
+	}
+
+	if err := c.Service.CreateFlashcard(uint(articleID), createRequest.Answer, createRequest.Question, createRequest.Tries, createRequest.Wrong); err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error creating flashcard"})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "Flashcard created successfully"})
+}
+
+func (c *ArticleFlashcardController) UpdateFlashcard(ctx *fiber.Ctx) error {
+	flashcardID, err := strconv.ParseUint(ctx.Params("id"), 10, 64)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid flashcard ID"})
+	}
+
+	var updatedFlashcard models.ArticleFlashcards
+	if err := ctx.BodyParser(&updatedFlashcard); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
+
+	if err := c.Service.UpdateFlashcard(uint(flashcardID), updatedFlashcard); err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error updating flashcard"})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Flashcard updated successfully"})
+}
+
+func (c *ArticleFlashcardController) DeleteFlashcard(ctx *fiber.Ctx) error {
+	flashcardID, err := strconv.ParseUint(ctx.Params("id"), 10, 64)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid flashcard ID"})
+	}
+
+	if err := c.Service.DeleteFlashcard(uint(flashcardID)); err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error deleting flashcard"})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Flashcard deleted successfully"})
+}
